07a078c5871c59f4475eb8f851a1744f7b79ed06
MCNorthwood <HotBlockMC@gmail.com>
2016-12-21 14:07:04 +0000

    Additions
    Small additions, will build on from this and get more added before
    testing the app

--- com.cet325.bg51az/app/src/main/java/com/example/bg51az/comcet325bg51az/DBOpenHelper.java
+++ com.cet325.bg51az/app/src/main/java/com/example/bg51az/comcet325bg51az/DBOpenHelper.java
@@ -7,6 +7,9 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by MCNorthwood on 11/12/2016.
  */
@@ -53,7 +56,7 @@ public class DBOpenHelper extends SQLiteOpenHelper {
                 KEY_FAVOURITE + " TEXT, " +
                 KEY_PLANNED + " TEXT, " +
                 KEY_VISITED + " TEXT, " +
-                KEY_IMAGE + " BLOB, " +
+                KEY_IMAGE + " TEXT, " +
                 KEY_GEOLOCATION + " TEXT, " +
                 KEY_PRICE + " DOUBLE, " +
                 KEY_RANK + " INTEGER )";
@@ -131,13 +134,135 @@ public class DBOpenHelper extends SQLiteOpenHelper {
             tourist.favourite = Boolean.parseBoolean(cursor.getString(3));
             tourist.planned = cursor.getString(4);
             tourist.visited = cursor.getString(5);
-            tourist.image = cursor.getBlob(6);
+            tourist.image = cursor.getString(6);
             tourist.geolocation = cursor.getString(7);
             tourist.price = cursor.getDouble(8);
             tourist.rank = cursor.getInt(9);
 
             Log.d("getTourist(" + id + ")", tourist.toString());
         }
+        cursor.close();
+        //5. return tourist
+        return tourist;
+    }
+
+    public List<Tourist> getAllTourists(){
+        List<Tourist> tourists =  new LinkedList<Tourist>();
+
+        //1. build the query
+        String query = "SELECT  * FROM " + TABLE_TOURIST;
+
+        //2. get reference to writable DB
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+
+        //3. go over each row, build tracks and add it to list
+        Tourist tourist = null;
+        if(cursor != null && cursor.moveToFirst()){
+            do{
+                tourist = new Tourist();
+                tourist.id = cursor.getInt(0);
+                tourist.name = cursor.getString(1);
+                tourist.location = cursor.getString(2);
+                tourist.favourite = Boolean.parseBoolean(cursor.getString(3));
+                tourist.planned = cursor.getString(4);
+                tourist.visited = cursor.getString(5);
+                tourist.image = cursor.getString(6);
+                tourist.geolocation = cursor.getString(7);
+                tourist.price = cursor.getDouble(8);
+                tourist.rank = cursor.getInt(9);
+
+                //Add tourist to tourists
+                tourists.add(tourist);
+            }while(cursor.moveToNext());
+        }
+        Log.d("getAllTracks()", tourists.toString());
+
+        cursor.close();
+        //return tourists
+        return tourists;
+    }
+
+    //Update single tourist
+    public int updateTourist(Tourist tourist){
+        //1. get reference to writable DB
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        //2. create ContentValues to add key "column"/value
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, tourist.name); //get name
+        values.put(KEY_LOCATION, tourist.location); //get location
+        values.put(KEY_DESCRIPTION, tourist.description); //get description
+        values.put(KEY_FAVOURITE, tourist.favourite); //get favourite
+        values.put(KEY_PLANNED, tourist.planned); //get planned
+        values.put(KEY_VISITED, tourist.visited); //get visited
+        values.put(KEY_IMAGE, tourist.image); //get image
+        values.put(KEY_GEOLOCATION, tourist.geolocation); //get geolocation
+        values.put(KEY_PRICE, tourist.price); //get price
+        values.put(KEY_RANK, tourist.rank); //get rank
+
+        //3. updating row
+        int i = db.update(TABLE_TOURIST, //table
+                values, //column/value
+                KEY_ID + " = ?", //selections
+                new String[] {String.valueOf(tourist.id)});//selection args
+
+        //4. close
+        db.close();
+
+        return i;
+    }
+
+    //Delete single tourist
+    public void deleteTourist(Tourist tourist){
+        //1. get reference to writable DB
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        //2. delete
+        db.delete(TABLE_TOURIST,
+                KEY_ID + " = ?",
+                new String[]{String.valueOf(tourist.id)});
+
+        //3. close
+        db.close();
+
+        Log.d("deleteTrack", tourist.toString());
+    }
+
+    public Tourist getTouristByName(String name){
+        //1. get reference to readable DB
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        //2. build query
+        Cursor cursor =
+                db.query(TABLE_TOURIST, //a. table
+                        COLUMNS, //b. column names
+                        KEY_NAME + " = ?", //c. selections
+                        new String[] {name}, //d. selection args
+                        null, //e. group by
+                        null, //f. having
+                        null, //g. order by
+                        null); //h. limit
+
+        //3. if a match was made
+        Tourist tourist = null;
+        if(cursor != null && cursor.moveToFirst()){
+            //4. build tourist object
+            tourist = new Tourist();
+            tourist.id = cursor.getInt(0);
+            tourist.name = cursor.getString(1);
+            tourist.location = cursor.getString(2);
+            tourist.favourite = Boolean.parseBoolean(cursor.getString(3));
+            tourist.planned = cursor.getString(4);
+            tourist.visited = cursor.getString(5);
+            tourist.image = cursor.getString(6);
+            tourist.geolocation = cursor.getString(7);
+            tourist.price = cursor.getDouble(8);
+            tourist.rank = cursor.getInt(9);
+
+            Log.d("getName(" +name+ ")", tourist.toString());
+        }
+
         cursor.close();
         //5. return tourist
         return tourist;
--- com.cet325.bg51az/app/src/main/java/com/example/bg51az/comcet325bg51az/Tourist.java
+++ com.cet325.bg51az/app/src/main/java/com/example/bg51az/comcet325bg51az/Tourist.java
@@ -11,7 +11,7 @@ public class Tourist {
     public Boolean favourite;
     public String planned;
     public String visited;
-    public byte[] image;
+    public String image;
     public String geolocation;
     public double price;
     public int rank;
@@ -21,7 +21,7 @@ public class Tourist {
     }
 
     //Change images to String ImageName, Object of Bitmap to store image into
-    public Tourist(String name, String loc, String des, Boolean fav, String plan, String visit, byte[] im, String geo, double price, int rank){
+    public Tourist(String name, String loc, String des, Boolean fav, String plan, String visit, String im, String geo, double price, int rank){
         this.name = name;
         this.location = loc;
         this.description = des;
@@ -47,4 +47,28 @@ public class Tourist {
             "\nPrice: " + price +
             "\nRank: " + rank);
     }
+
+    // set
+    public void setName(String pName){name = pName;}
+    public void setLocation(String pLocation){location = pLocation;}
+    public void setDescription(String pDescription){description = pDescription;}
+    public void setFavourite(boolean pFavourite){favourite = pFavourite;}
+    public void setPlanned(String pPlanned){planned = pPlanned;}
+    public void setVisited(String pVisited){visited = pVisited;}
+    public void setImage(String pImage){image = pImage;}
+    public void setGeolocation(String pGeolocation){geolocation = pGeolocation;}
+    public void setPrice(double pPrice){price = pPrice;}
+    public void setRank(int pRank){rank = pRank;}
+
+    //get
+    public String getName(){return name;}
+    public String getLocation(){return location;}
+    public String getDescription(){return description;}
+    public Boolean getFavourite(){return favourite;}
+    public String getPlanned(){return planned;}
+    public String getVisited(){return visited;}
+    public String getImage(){return image;}
+    public String getGeolocation(){return geolocation;}
+    public double getPrice(){return price;}
+    public int getRank(){return rank;}
 }
